b6dd07fb60f922f5037d906077228026b2fe8e4b
Chaleira <pedromchaleira@gmail.com>
2025-03-20 11:23:22 +0000

    Delete old frontend and updated expo package

--- frontend_old/app/(main)/playlist.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function PlaylistScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24 }}>Playlist Screen</Text>
-    </View>
-  );
-}
