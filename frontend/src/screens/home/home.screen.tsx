b5248fd3be6288aa5052e3b72f4944165c349836
Chaleira <pedromchaleira@gmail.com>
2025-03-20 12:07:05 +0000

    Home Screen done for now, waiting for new design

--- frontend/src/screens/home/home.screen.tsx
+++ frontend/src/screens/home/home.screen.tsx
@@ -1,49 +1,67 @@
-import React from 'react';
-import { View, Text, Alert, TouchableOpacity, Image } from 'react-native';
-import MyButton from 'components/MyButton';
-import BottomNav from 'components/BottomNav';
-import { getMainData } from '@api/user.api';
-import { useAuth } from '@hooks/auth.hooks';
-import { useHome } from '@hooks/home.hooks';
-import { FlatList } from 'react-native-gesture-handler';
-import { useAudioPlayer } from '@hooks/player.hooks';
+import React from "react";
+import { View, Text, Alert, TouchableOpacity, Image } from "react-native";
+import MyButton from "components/MyButton";
+import BottomNav from "components/BottomNav";
+import { getMainData } from "@api/user.api";
+import { useAuth } from "@hooks/auth.hooks";
+import { useHome } from "@hooks/home.hooks";
+import { FlatList } from "react-native-gesture-handler";
+import { useAudioPlayer } from "@hooks/player.hooks";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }: any) {
-  const { user, logout } = useAuth();
-  const { songs } = useHome();
-  const { selectTrack } = useAudioPlayer();
+	const { user, logout } = useAuth();
+	const { songs } = useHome();
+	const { selectTrack } = useAudioPlayer();
 
-  const handleGet = async () => {
-		try{
+	const handleGet = async () => {
+		try {
 			const data = await getMainData();
-			Alert.alert('Get', data);
+			Alert.alert("Get", data);
 		} catch (error: any) {
-			Alert.alert('Get', error.message);
+			Alert.alert("Get", error.message);
 		}
-	}
+	};
 
-  return (
-	  <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingTop:35 }}>
-      		{/* <Text style={{ fontSize: 24 }}>Welcome {user?.usejrname} !</Text> */}
-      		<Text style={{ fontSize: 24 }}>Home Screen</Text>
-		<View style={{ flex: 1, paddingTop: 0, alignItems: "center" }}>
-		<FlatList data={songs} keyExtractor={(item) => item.fileId} renderItem={({ item }) => (
-			<TouchableOpacity onPress={async () => { await selectTrack(item, navigation)}}>
-				<View style={{ flexDirection: "row", padding: 10, alignItems: "center", paddingLeft: 0 }}>
-					<Image source={{ uri: item.thumbnail }} style={{ width: 100, height: 70, borderRadius: 8, marginRight: 10 }} />
-					<Text style={{ fontSize: 16, flexShrink: 1 }}>{item.title}</Text>
-				</View>
-			</TouchableOpacity>
-		)}>
-		</FlatList>
+	return (
+		<View style={{ flex: 1, paddingLeft: 10 }}>
+			<View style={{ flexDirection: "row", paddingTop: 35, alignItems: "flex-end" }}>
+				<Text style={{ fontSize: 24 }}>Lastly Played</Text>
+				<TouchableOpacity
+					onPress={handleGet}
+					style={{ marginLeft: "auto", paddingRight: 10 }}
+				>
+					<Ionicons name="help" size={28} />
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => logout()}
+					style={{ paddingRight: 10 }}
+				>
+					<Ionicons name="log-out" size={28} />
+				</TouchableOpacity>
+				{/* <Text style={{ fontSize: 24 }}>Lastly Played</Text> */}
 			</View>
-		<View style={{paddingBottom: 50, flexDirection: "row"}}>
-			<MyButton title="Logout" onPress={() => logout()} />
-			<MyButton title="Get" onPress={handleGet} />
-			{/* <MyButton title="Player Screen" onPress={()=>{navigation.navigate('Player')}} /> */}
+			<FlatList
+				data={songs}
+				keyExtractor={(item) => item.fileId}
+				renderItem={({ item }) => (
+					<TouchableOpacity
+						onPress={async () => {
+							await selectTrack(item, navigation);
+						}}
+					>
+						<View style={{ flexDirection: "row", padding: 10, alignItems: "center", paddingLeft: 0 }}>
+							<Image source={{ uri: item.thumbnail }} style={{ width: 100, height: 70, borderRadius: 8, marginRight: 10 }} />
+							<Text style={{ fontSize: 16, flexShrink: 1 }}>{item.title}</Text>
+						</View>
+					</TouchableOpacity>
+				)}
+			></FlatList>
+			{/* <View style={{ paddingBottom: 50, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+				<MyButton title="Logout" onPress={() => logout()} />
+				<MyButton title="Get" onPress={handleGet} />
+			</View> */}
+			<BottomNav navigation={navigation} active="Home" />
 		</View>
-		<BottomNav navigation={navigation} active="Home" />
-	</View>
-	
-  );
+	);
 }
